33bc6e27db83e86912fb600695e81fd508c714f1
Jedidiah Abera <yididi@Jedidiah>
2024-05-24 22:06:09 +0300

    solved find word's containing character

---
+++ LeetCode-Python/12-2942. Find Words Containing Character.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findWordsContaining(self, words, x):
+        """
+        :type words: List[str]
+        :type x: str
+        :rtype: List[int]
+        """
+        result = []
+
+        for i, word in enumerate(words):
+            if x in word:
+                result.append(i)
+        return result
