ecf52c4d512ff193812d5392a06c196e8bc8cae7
Jedidiah Abera <yididi@Jedidiah>
2024-05-26 18:37:50 +0300

    Solved Kids with the greatest number of candies

---
+++ LeetCode-Python/17-1431. Kids With the Greatest Number of Candies.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def kidsWithCandies(self, candies, extraCandies):
+        """
+        :type candies: List[int]
+        :type extraCandies: int
+        :rtype: List[bool]
+        """
+        max_value = max(candies)
+
+        return [i + extraCandies >= max_value for i in candies]
