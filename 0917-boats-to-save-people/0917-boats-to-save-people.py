eb0a1612f9be9bdf5b1e1651101db8f0333d8061
Yididia Abera <140195943+yididiabera@users.noreply.github.com>
2024-11-29 14:10:57 +0300

    Time: 75 ms (8%), Space: 16.2 MB (41.28%) - LeetHub

--- 0917-boats-to-save-people/0917-boats-to-save-people.py
+++ 0917-boats-to-save-people/0917-boats-to-save-people.py
@@ -5,11 +5,14 @@ class Solution(object):
         :type limit: int
         :rtype: int
         """
+        #sort the people to easily find the lightest and the heaviest person
+        #use two ptrs and pair the lightest and the heaviest person, if it exceeds the limit, use the boat for only the heaviest person
+
         people.sort()
         l, r = 0, len(people) - 1
         boat = 0
 
-        while(l <= r):
+        while l <= r:
             if people[l] + people[r] <= limit:
                 l += 1
             r -= 1
