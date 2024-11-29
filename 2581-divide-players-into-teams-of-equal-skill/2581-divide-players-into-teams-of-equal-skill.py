9b6c226a3f15502bddf604695e207ba7c52c8a2b
Yididia Abera <140195943+yididiabera@users.noreply.github.com>
2024-11-29 12:01:54 +0300

    Time: 55 ms (47.15%), Space: 20.2 MB (96.59%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.py
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.py
@@ -4,11 +4,14 @@ class Solution(object):
         :type skill: List[int]
         :rtype: int
         """
+        #first sort the skill
+        #intialize left and right ptrs one at the beginning of the array and one at the end the array
+        #compute target skill sum using the new two ptrs
+        #pair players and check if the violate the condition(not equal to our target) if not add the product to the total chemistry
         skill.sort()
         l, r = 0, len(skill) - 1
-        total_chemistry = 0
-
         target = skill[l] + skill[r]
+        total_chemistry = 0
 
         while l < r:
             if skill[l] + skill[r] != target:
@@ -16,5 +19,4 @@ class Solution(object):
             total_chemistry += skill[l] * skill[r]
             l += 1
             r -= 1
-
         return total_chemistry
