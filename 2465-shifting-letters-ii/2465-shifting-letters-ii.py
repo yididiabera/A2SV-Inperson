5acc5e03481bb75b9bfcccc04764c71c5ab64658
Yididia Abera <140195943+yididiabera@users.noreply.github.com>
2024-12-06 15:01:32 +0300

    Time: 83 ms (76.47%), Space: 30.2 MB (39.13%) - LeetHub

--- 2465-shifting-letters-ii/2465-shifting-letters-ii.py
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.py
@@ -5,28 +5,34 @@ class Solution(object):
         :type shifts: List[List[int]]
         :rtype: str
         """
+        #create a dif array - with +1 size to hold the amount of shifts on each char
+        # +1 because as a flag to know where the shifting ends
+        #calculate the prefix sum of the dif
+        #and shift the string accordingly
+
+        s = list(s)
         n = len(s)
-        shift_effect = [0] * (n + 1)
-
-        # Apply each shift to the shift_effect array
+        dif = [0] * (n + 1)
         for start, end, direction in shifts:
-            delta = 1 if direction == 1 else -1
-            shift_effect[start] += delta
-            if end + 1 < n:
-                shift_effect[end + 1] -= delta
-
-        # Compute cumulative shifts
-        cumulative_shift = 0
+            if direction == 1:
+                dif[start] += 1
+                dif[end + 1] -= 1
+            else:
+                dif[start] -= 1
+                dif[end + 1] += 1
+        
+        for i in range(1, len(dif)):
+            dif[i] = dif[i - 1] + dif[i]
+        
         for i in range(n):
-            cumulative_shift += shift_effect[i]
-            shift_effect[i] = cumulative_shift
-        
-        # Apply shifts to the string
-        result = []
-        for i, char in enumerate(s):
-            # Compute new character with wrapping
-            shift = shift_effect[i] % 26
-            new_char = chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
-            result.append(new_char)
-        
-        return ''.join(result)
+            s[i] = chr((ord(s[i]) - ord('a') + dif[i]) % 26 + ord('a'))
+        return ''.join(s)
+
+        # s = list(s)
+        # for start, end, direction in shifts:
+        #     for i in range(start, end + 1):
+        #         if direction == 1:
+        #             s[i] = chr(((ord(s[i]) - ord('a') + 1) % 26) + ord('a'))
+        #         else:
+        #             s[i] = chr(((ord(s[i]) - ord('a') - 1) % 26) + ord('a'))
+        # return "".join(s)
