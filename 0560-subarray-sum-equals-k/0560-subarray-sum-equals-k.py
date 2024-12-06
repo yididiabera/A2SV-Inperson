2e096fb73bccef5d98c8f31694ee9ceb798c5204
Yididia Abera <140195943+yididiabera@users.noreply.github.com>
2024-12-06 20:56:02 +0300

    Time: 34 ms (51.29%), Space: 19.9 MB (24.6%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,15 +1,30 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        curSum = 0
+        #iterate through the number and calculate the running sum
+        #check if the curSum - k equals to the prev subarray
+        #if so there exists a sum of k between the pt and the current index
+        #use hashmap to hold the frequency of the prefix sum
+        prefix = {0:1}
         count = 0
-        prefixSums = {0 : 1}
+        _sum = 0
 
-        for num in nums:
-            curSum += num
-            dif = curSum - k
-            count += prefixSums.get(dif, 0)
-            prefixSums[curSum] = prefixSums.get(curSum, 0) + 1
+        for i in range(len(nums)):
+            _sum += nums[i]
+            dif = _sum - k
+            if dif in prefix:
+                count += prefix[dif]
+            prefix[_sum] = prefix.get(_sum, 0) + 1
         return count
+        # curSum = 0
+        # count = 0
+        # prefixSums = {0 : 1}
+
+        # for num in nums:
+        #     curSum += num
+        #     dif = curSum - k
+        #     count += prefixSums.get(dif, 0)
+        #     prefixSums[curSum] = prefixSums.get(curSum, 0) + 1
+        # return count
         
         # l = 0
         # count = 0
