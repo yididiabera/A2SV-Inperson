29e5abda755cbdd4bbc05a6c3ee374e3edb5e9e6
Yididia Abera <140195943+yididiabera@users.noreply.github.com>
2024-12-06 16:22:44 +0300

    Time: 134 ms (62.79%), Space: 20.4 MB (68.42%) - LeetHub

--- 0878-shifting-letters/0878-shifting-letters.py
+++ 0878-shifting-letters/0878-shifting-letters.py
@@ -6,14 +6,29 @@ class Solution(object):
         :rtype: str
         """
         s = list(s)
-        n = len(shifts)
-        dif = [0] * n
-        _sum = sum(shifts)
-        for i in range(n):
-            dif[i] = _sum
-            s[i] = chr((ord(s[i]) - ord('a') + dif[i]) % 26 + ord('a'))
-            _sum = _sum - shifts[i]
+        n = len(s)
+        cum_shift = 0
+        for i in range(n - 1, -1, -1):
+            cum_shift += shifts[i]
+            cum_shift %= 26
+            s[i] = chr((ord(s[i]) - ord('a') + cum_shift) % 26 + ord('a'))
         return "".join(s)
+
+        #---------------------
+        
+        # s = list(s)
+        # n = len(shifts)
+        # dif = [0] * n
+        # _sum = sum(shifts)
+        # for i in range(n):
+        #     dif[i] = _sum
+        #     s[i] = chr((ord(s[i]) - ord('a') + dif[i]) % 26 + ord('a'))
+        #     _sum = _sum - shifts[i]
+        # return "".join(s)
+
+        #-------------------
+
+
         #time limit exceeded
         # s = list(s)
         # for i in range(len(shifts)):
