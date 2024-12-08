79de9210bd5868a7370cf7b5281b88d0ddd94810
Yididia Abera <140195943+yididiabera@users.noreply.github.com>
2024-12-08 22:01:53 +0300

    Time: 658 ms (81.78%), Space: 18 MB (30.9%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,23 +4,34 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        nums.sort()
+        n = len(nums)
         result = []
-
+        nums.sort()
         for i, value in enumerate(nums):
-            if i > 0 and nums[i - 1] == value:
+            if i > 0 and value == nums[i - 1]:
                 continue
-            
-            l, r = i + 1, len(nums) - 1
-            while(l < r):
+            l, r = i + 1, n - 1
+
+            while l < r:
                 threeSum = value + nums[l] + nums[r]
                 if threeSum < 0:
                     l += 1
                 elif threeSum > 0:
                     r -= 1
-                else: 
+                else:
                     result.append([value, nums[l], nums[r]])
                     l += 1
                     while nums[l] == nums[l - 1] and l < r:
                         l += 1
         return result
+        # result = []
+        # n = len(nums)
+
+        # for i in range(n):
+        #     for j in range(i + 1, n):
+        #         for k in range(j + 1, n):
+        #             if nums[i] + nums[j] + nums[k] == 0:
+        #                 triplet = sorted([nums[i], nums[j], nums[k]])
+        #                 if triplet not in result:
+        #                     result.append(triplet)
+        # return result
