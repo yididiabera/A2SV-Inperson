70638cca9bd9ff2ffa042ef61144b0d3905bd9a5
Yididia Abera <140195943+yididiabera@users.noreply.github.com>
2024-12-12 23:53:38 +0300

    Time: 19 ms (33.84%), Space: 12.5 MB (7.2%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,22 +1,22 @@
-class Solution:
-    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        # nums1 = [4, 1, 2]   nums2 = [1, 3, 4, 2]
-        n = len(nums1)
+class Solution(object):
+    def nextGreaterElement(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        nums1Idx = {}
+        for i, value in enumerate(nums1):
+            nums1Idx[value] = i
+        
+        result = [-1] * len(nums1)
         m = len(nums2)
-        result = []
-        for i in range(n):
-            found = False
-            for j in range(m):
-                if nums1[i] == nums2[j]:
-                    index = j
-                    while index < m:
-                        if nums2[index] > nums1[i]:
-                            result.append(nums2[index])
-                            found = True
-                            break
-                        index += 1
-                        
-                    if not found:
-                        result.append(-1)
-                    # break
+        for i in range(m):
+            if nums2[i] not in nums1Idx:
+                continue
+            for j in range(i + 1, m):
+                if nums2[j] > nums2[i]:
+                    idx = nums1Idx[nums2[i]]
+                    result[idx] = nums2[j]
+                    break
         return result
