c07327aa9a491307ef769bb0a2f3920db4b1c776
Yididia Abera <140195943+yididiabera@users.noreply.github.com>
2024-12-16 22:22:27 +0300

    Time: 15 ms (77.17%), Space: 17.6 MB (10.1%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,11 +1,11 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        countM = {}
-        countR = {}
-        for c in magazine:
-            countM[c] = countM.get(c, 0) + 1
-        for c in ransomNote:
-            countR[c] = countR.get(c, 0) + 1
+        countM = Counter(magazine)
+        countR = Counter(ransomNote)
+        # for c in magazine:
+        #     countM[c] = countM.get(c, 0) + 1
+        # for c in ransomNote:
+        #     countR[c] = countR.get(c, 0) + 1
         
         for c in ransomNote:
             if countR[c] > countM.get(c, 0):
