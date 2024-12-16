dfc297a9fca26381d3f399a06aa6d81f6a7a28c0
Yididia Abera <140195943+yididiabera@users.noreply.github.com>
2024-12-16 14:33:47 +0300

    Time: 42 ms (24.82%), Space: 18.6 MB (83.7%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -5,23 +5,13 @@ class Solution(object):
         :type k: int
         :rtype: bool
         """
-        if k == 0 or not nums:
-            return False
-        
-        window = {}
-        k = min(k, len(nums))  # Ensure k is within nums length
-
-        for i in range(k):
-            if nums[i] in window:
+        window = set()
+        l = 0
+        for r in range(len(nums)):
+            if nums[r] in window:
                 return True
-            window[nums[i]] = window.get(nums[i], 0) + 1
-        
-        for i in range(k, len(nums)):
-            if nums[i] in window:
-                return True
-            window[nums[i]] = window.get(nums[i], 0) + 1
-            window[nums[i - k]] -= 1
-            if window[nums[i - k]] == 0:
-                del window[nums[i - k]]
-        
+            window.add(nums[r])
+            if r - l + 1 > k:
+                window.remove(nums[l])
+                l += 1
         return False
