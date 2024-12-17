aa06a41315a15b4ee7fdcdc815be18383ae68775
Yididia Abera <140195943+yididiabera@users.noreply.github.com>
2024-12-17 11:30:03 +0300

    Time: 94 ms (72.63%), Space: 18.2 MB (11.3%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.py
@@ -6,25 +6,23 @@ class Solution(object):
         :rtype: int
         """
         count = 0
-        prefix = { 0: 1}
+        curSum = 0
+        prefix = {0:1}
         for i in range(len(nums)):
-            if nums[i] % 2 == 0:
-                nums[i] = 0
-            else:
-                nums[i] = 1
-        _sum = 0
-        for i in range(len(nums)):
-            _sum +=  nums[i]
-            dif = _sum - k
-            count += prefix.get(dif, 0)
-            prefix[_sum] = prefix.get(_sum, 0) + 1
+            if nums[i] % 2 == 1:
+                curSum += 1
+            
+            if curSum - k in prefix:
+                count += prefix[curSum - k]
+            prefix[curSum] = prefix.get(curSum, 0) + 1
         return count
-        # count = 0
+
+        # result = 0
         # for i in range(len(nums)):
-        #     oddCount = 0
+        #     count = 0
         #     for j in range(i, len(nums)):
         #         if nums[j] % 2 == 1:
-        #             oddCount += 1
-        #         if oddCount == k:
         #             count += 1
-        # return count
+        #         if count == k:
+        #             result += 1
+        # return result
