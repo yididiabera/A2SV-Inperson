deffae53c9fd1394fc71ef22b3cb655779e33939
Yididia Abera <140195943+yididiabera@users.noreply.github.com>
2024-12-18 23:19:21 +0300

    Time: 4 ms (26.57%), Space: 17.8 MB (10.4%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,14 +1,20 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        sHash = {}
-        tHash = {}
-
-        for c in s:
-            sHash[c] = sHash.get(c, 0) + 1
+        sHash = Counter(s)
         for c in t:
-            tHash[c] = tHash.get(c, 0) + 1
-        for c in t:
-            if c not in sHash or tHash[c] > sHash[c]:
+            if sHash[c] == 0:
                 return c
-                 
+            sHash[c] -= 1
+
+        # sHash = {}
+        # tHash = {}
+
+        # for c in s:
+        #     sHash[c] = sHash.get(c, 0) + 1
+        # for c in t:
+        #     tHash[c] = tHash.get(c, 0) + 1
+        # for c in t:
+        #     if c not in sHash or tHash[c] > sHash[c]:
+        #         return c
+
             
