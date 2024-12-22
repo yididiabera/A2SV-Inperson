331a52bdd78840936fb2b535818578ff35d1e72d
Yididia Abera <140195943+yididiabera@users.noreply.github.com>
2024-12-22 17:57:49 +0300

    Time: 3 ms (50.21%), Space: 12.5 MB (7.28%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -4,16 +4,23 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        #create a dict to match an opening to closing tag
+        #create a stack to hold the values of the opening tag
+        #iterate therough the string and add to stack if the parentehese is opening
+        #and remove from the stack the opening parenthese when you encounter the closing parenthese
+        #return true if the stack is empty
+        matches = {')':'(', ']':'[', '}':'{'}
         stack = []
-        matching = {')':'(', '}': '{', ']':'['}
 
         for c in s:
-            if c in matching.values():
+            if c in matches.values():
                 stack.append(c)
-            elif c in matching.keys():
-                if not stack or stack[-1] != matching[c]:
+            elif c in matches.keys():
+                if stack and matches[c] == stack[-1]:
+                    stack.pop()
+                else:
                     return False
-                stack.pop()
             else:
                 return False
         return not stack
+            
