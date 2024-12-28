7cfbaf02279b23b8c1d1ed5f4ff6d98a3097b642
Yididia Abera <140195943+yididiabera@users.noreply.github.com>
2024-12-28 11:07:24 +0300

    Time: 3 ms (2.31%), Space: 17.7 MB (10.19%) - LeetHub

--- 2491-smallest-even-multiple/2491-smallest-even-multiple.py
+++ 2491-smallest-even-multiple/2491-smallest-even-multiple.py
@@ -1,10 +1,13 @@
 class Solution:
     def smallestEvenMultiple(self, n: int) -> int:
-        def GCD(dividend, divisor):
-            while divisor != 0:
-                dividend, divisor = divisor, dividend % divisor
-            return dividend
-        dividend = max(2, n)
-        divisor = min(2, n)
-        result = (dividend * divisor) // GCD(dividend, divisor)
-        return result
+        if n % 2 == 0:
+            return n
+        return 2 * n
+        # def GCD(dividend, divisor):
+        #     while divisor != 0:
+        #         dividend, divisor = divisor, dividend % divisor
+        #     return dividend
+        # dividend = max(2, n)
+        # divisor = min(2, n)
+        # result = (dividend * divisor) // GCD(dividend, divisor)
+        # return result
