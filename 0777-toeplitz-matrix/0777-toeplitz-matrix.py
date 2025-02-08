2cb90529fda6d498b07313ec93193354ce4ebe2a
Yididia Abera <yididiabera87@gmail.com>
2025-02-08 08:17:36 +0300

    Time: 0 ms (100%), Space: 17.9 MB (26.47%) - LeetHub

--- 0777-toeplitz-matrix/0777-toeplitz-matrix.py
+++ 0777-toeplitz-matrix/0777-toeplitz-matrix.py
@@ -2,19 +2,19 @@ class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
         #iterate through the matrix starting from the row idx 1 and column idx 1
         #check whether the element is equal to element found idx - 1 (top left element)
-        prev_row = None
-        for cur_row in matrix:
-            if prev_row:
-                for j in range(1, len(cur_row)):
-                    if cur_row[j] != prev_row[j - 1]:
-                        return False
-            prev_row = cur_row
-        return True
-
-
-
-        # for i in range(1, len(matrix)):
-        #     for j in range(1, len(matrix[i])):
-        #         if matrix[i][j] != matrix[i - 1][j - 1]:
-        #             return False
+        # prev_row = None
+        # for cur_row in matrix:
+        #     if prev_row:
+        #         for j in range(1, len(cur_row)):
+        #             if cur_row[j] != prev_row[j - 1]:
+        #                 return False
+        #     prev_row = cur_row
         # return True
+
+
+
+        for i in range(1, len(matrix)):
+            for j in range(1, len(matrix[i])):
+                if matrix[i][j] != matrix[i - 1][j - 1]:
+                    return False
+        return True
