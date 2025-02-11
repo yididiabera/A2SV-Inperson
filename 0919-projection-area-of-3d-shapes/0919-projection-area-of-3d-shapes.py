f6733a613b50424b9f22852b84002dfb68d6c008
Yididia Abera <yididiabera87@gmail.com>
2025-02-11 11:13:42 +0300

    Time: 7 ms (18.05%), Space: 18.1 MB (16.11%) - LeetHub

--- 0919-projection-area-of-3d-shapes/0919-projection-area-of-3d-shapes.py
+++ 0919-projection-area-of-3d-shapes/0919-projection-area-of-3d-shapes.py
@@ -1,20 +1,32 @@
 class Solution:
     def projectionArea(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-        m = len(grid[0])
-        top = 0
-        right = 0
-        front = 0
-        front_max = [0] * n
+        ans = 0
+        for i in range(len(grid)):
+            row = 0
+            col = 0
+            for j in range(len(grid)):
+                row = max(row, grid[i][j])
+                col = max(col, grid[j][i])
+                if grid[i][j] > 0:
+                    ans += 1
+            ans += row + col
+        return ans
+
+        # n = len(grid)
+        # m = len(grid[0])
+        # top = 0
+        # right = 0
+        # front = 0
+        # front_max = [0] * n
         
-        for i in range(n):
-            right_max = 0
-            for j in range(m):
-                val = grid[i][j]
-                if val != 0:
-                    top += 1
-                right_max = max(right_max, val)
-                front_max[j] = max(front_max[j], val)
-            right += right_max
-        total = top + right + sum(front_max)
-        return total 
+        # for i in range(n):
+        #     right_max = 0
+        #     for j in range(m):
+        #         val = grid[i][j]
+        #         if val != 0:
+        #             top += 1
+        #         right_max = max(right_max, val)
+        #         front_max[j] = max(front_max[j], val)
+        #     right += right_max
+        # total = top + right + sum(front_max)
+        # return total 
