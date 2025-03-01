03672253a12e80dc7f26087f7b048742172bdc13
Yididia Abera <yididiabera87@gmail.com>
2025-03-01 22:35:34 +0300

    Time: 0 ms (100%), Space: 18.1 MB (29.54%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.py
@@ -1,36 +1,12 @@
-class Solution(object):
-    def applyOperations(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        #iterate over the entire array
-        #check if the current element and the previous one are equal or not
-        #if they are equal mulitply the prev element by 2 and make the current zero
-        n = len(nums)
-        l = 0
-        for r in range(n):
-            if r < n - 1 and nums[r] == nums[r + 1]:
-                nums[r] *= 2
-                nums[r + 1] = 0
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                nums[i], nums[i + 1] = nums[i] * 2, 0
+        
+        l,r = 0, 0
+        for r in range(len(nums)):
             if nums[r]:
-                nums[l] = nums[r]
-                if l != r:
-                    nums[r] = 0
+                nums[l], nums[r] = nums[r], nums[l]
                 l += 1
         return nums
-        
-        
-        # n = len(nums)
-        # l = 0
-        # for r in range(len(nums)):
-        #     if r < n - 1 and nums[r] == nums[r + 1]:
-        #         nums[r] *= 2
-        #         nums[r + 1] = 0
-            
-        #     if nums[r]:
-        #         nums[l] = nums[r]
-        #         if l != r:
-        #             nums[r] = 0
-        #         l += 1
-        # return nums
