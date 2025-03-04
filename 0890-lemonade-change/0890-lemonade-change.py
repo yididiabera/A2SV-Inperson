b1f1a5db8555a28fe192c22cfbe2aabb77a735cf
Yididia Abera <yididiabera87@gmail.com>
2025-03-04 18:07:29 +0300

    Time: 3 ms (78.46%), Space: 22 MB (35.76%) - LeetHub

---
+++ 0890-lemonade-change/0890-lemonade-change.py
@@ -0,0 +1,31 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        five_bills = 0
+        ten_bills = 0
+        twenty_bills = 0
+
+        for bill in bills:
+            if bill == 5:
+                five_bills += 1
+            elif bill == 10:
+                ten_bills += 1
+                if five_bills > 0:
+                    five_bills -= 1
+                else:
+                    return False
+            elif bill == 20:
+                if ten_bills > 0:
+                    ten_bills -= 1
+                    if five_bills > 0:
+                        five_bills -= 1
+                    else:
+                        return False
+                else:
+                    if five_bills > 2:
+                        five_bills -= 3
+                        bill -= 5
+                    else:
+                        return False
+
+        return True
+                
