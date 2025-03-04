b1ec18c62af7ca4ebc0091f71c068c8bd10486ff
Yididia Abera <yididiabera87@gmail.com>
2025-03-04 19:27:39 +0300

    Time: 3 ms (38.48%), Space: 17.7 MB (61.52%) - LeetHub

--- 3055-maximum-odd-binary-number/3055-maximum-odd-binary-number.py
+++ 3055-maximum-odd-binary-number/3055-maximum-odd-binary-number.py
@@ -1,20 +1,21 @@
 class Solution:
     def maximumOddBinaryNumber(self, s: str) -> str:
-        s_reversed= sorted(s, reverse=True)
+        count = Counter(s)
 
-        #print(st_reversed)
-        count = 0
-        for ch in s_reversed:
-            if ch == '1':
-                count += 1
-        if count < 1:
-            s.sort()
-            return "".join(s)
-        else:
-            for i in range(len(s) - 1, -1, -1):
-                if s_reversed[i] == '1':
-                    s_reversed[i], s_reversed[-1] = s_reversed[-1], s_reversed[i]
-                    break
-        return "".join(s_reversed)
+        return "".join(['1'] * (count['1'] - 1) + ['0'] * count['0'] + ['1'] )
+        # s_reversed= sorted(s, reverse=True)
+        # count = 0
+        # for ch in s_reversed:
+        #     if ch == '1':
+        #         count += 1
+        # if count < 1:
+        #     s.sort()
+        #     return "".join(s)
+        # else:
+        #     for i in range(len(s) - 1, -1, -1):
+        #         if s_reversed[i] == '1':
+        #             s_reversed[i], s_reversed[-1] = s_reversed[-1], s_reversed[i]
+        #             break
+        # return "".join(s_reversed)
 
 
