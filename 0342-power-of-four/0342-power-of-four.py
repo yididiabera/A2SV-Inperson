39b00cb4c2183bde9b135d315cdd7c7e1ff55850
Yididia Abera <yididiabera87@gmail.com>
2025-03-11 19:00:03 +0300

    Time: 0 ms (100%), Space: 12.5 MB (51.12%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isPowerOfFour(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0:
+            return False
+    
+        while n % 4 == 0:
+            n //= 4
+
+        if n == 1:
+            return True
+        return False
