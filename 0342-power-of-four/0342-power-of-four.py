a5fc069b1923dff12f9ddd7140f3fb6df3193d6c
Yididia Abera <yididiabera87@gmail.com>
2025-03-12 14:53:30 +0300

    Time: 0 ms (100%), Space: 12.5 MB (10.04%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -4,10 +4,13 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        if n <= 0:
+        if n == 1:
+            return True
+        if n == 0:
             return False
-    
+            
         while n % 4 == 0:
-            n //= 4
-
+            print(n // 4)
+            n /= 4
+        
         return n == 1
