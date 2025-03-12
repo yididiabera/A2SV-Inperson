2fda688fdcdad03d81ced14831bac46f6acc446a
Yididia Abera <yididiabera87@gmail.com>
2025-03-12 17:41:37 +0300

    Time: 0 ms (100%), Space: 12.5 MB (54.39%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -4,14 +4,14 @@ class Solution(object):
         :type rowIndex: int
         :rtype: List[int]
         """
-        result = [1]
-        temp = [0]
-        for i in range(rowIndex):
-            temp = [0] * (len(result) + 1)
-            for j in range(len(result)):
-                temp[j] += result[j]
-                temp[j + 1] += result[j]
-            result = temp
-        return result
+        result = [[1]]
         
-            
+        for i in range(1, rowIndex + 1):
+            temp = [0] + result[-1] + [0]
+            new_row = [0] * (len(result[-1]) + 1)
+
+            for j in range(len(new_row)):
+                new_row[j] = temp[j] + temp[j + 1]
+            result.append(new_row)
+
+        return result[-1]
