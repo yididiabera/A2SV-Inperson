18dd50c5a74abcacb9a0b1bcbc4b427257ac45f5
Yididia Abera <yididiabera87@gmail.com>
2025-03-13 16:17:41 +0300

    Time: 0 ms (100%), Space: 17.8 MB (55.23%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,23 +1,36 @@
-class Solution(object):
-    def decodeString(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
+class Solution:
+    def decodeString(self, s: str) -> str:
+        # 3 [ a 2 [ c ] ]
+        # 0 1 2 3 4 5 6 7
+
+        close = [0] * len(s)
         stack = []
-        for c in s:
-            if c != ']':
-                stack.append(c)
-            else:
-                subStr = ""
-                while stack[-1] != "[":
-                    subStr = stack.pop() + subStr
-                stack.pop()
-                
-                k = ""
-                while stack and stack[-1].isdigit():
-                    k = stack.pop() + k
-                
-                stack.append(int(k) * subStr)
+        for i in range(len(s)):
+            if s[i] == '[':
+                stack.append(i)
+            elif s[i] == ']':
+                close[stack.pop()] = i
         
-        return "".join(stack)
+        def decode(i, j):
+            nonlocal s
+            nonlocal close
+            num = ''
+            ans = ''
+
+            while i < j:
+                if s[i].isdigit():
+                    num += s[i]
+                    #i += 1
+                
+                elif s[i].isalpha():
+                    ans += s[i]
+                    #i += 1
+                
+                else:
+                    ans += int(num) * decode(i + 1, close[i])
+                    i = close[i]
+                    num  = ""
+                
+                i += 1
+            return ans
+        return decode(0, len(s))
